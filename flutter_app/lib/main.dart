bb6109901738dadd2be779c0967d681469ffd6e1
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 09:18:28 +0530

    Margin for container

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -104,6 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Container(
           height: double.infinity, // Take as much as place possible for height
           width: double.infinity,
+          margin: EdgeInsets.all(50.0),
           padding: EdgeInsets.all(50.0),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(25.0),
