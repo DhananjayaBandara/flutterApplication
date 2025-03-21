7c884bf1cb665f86f130815c62c667e4cf06aa7e
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 09:20:40 +0530

    Wrap the container with column by right click on Container widget,goto refactor,select wrap with column

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -101,27 +101,20 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Container(
-          height: double.infinity, // Take as much as place possible for height
-          width: double.infinity,
-          margin: EdgeInsets.all(50.0),
-          padding: EdgeInsets.all(50.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25.0),
-            color: Colors.red,
-          ),
-          child: Container(
-            height:
-                double.infinity, // Take as much as place possible for height
-            width: double.infinity,
-            margin: EdgeInsets.all(50.0),
-            padding: EdgeInsets.all(50.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25.0),
-              color: Colors.orange,
+        child: Column(
+          children: [
+            Container(
+              height:
+                  double.infinity, // Take as much as place possible for height
+              width: double.infinity,
+              margin: EdgeInsets.all(50.0),
+              padding: EdgeInsets.all(50.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.red,
+              ),
             ),
-            child: Text('Hello', style: TextStyle(color: Colors.black)),
-          ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
