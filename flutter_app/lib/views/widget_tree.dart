1b129194b49ca73ce0dab4d27d0198fa33cf53b5
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 15:17:17 +0530

    Handling widget tree

--- flutter_app/lib/views/widget_tree.dart
+++ flutter_app/lib/views/widget_tree.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/pages/home_page.dart';
+import 'package:flutter_app/views/pages/profile_page.dart';
 import 'package:flutter_app/widgets/nav_bar_widget.dart';
+
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -8,6 +12,7 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter App'), centerTitle: true),
+      body: pages.elementAt(1),
       bottomNavigationBar: NavBarWidget(),
     );
   }
