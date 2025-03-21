67edd24eede44145d2e76e3fc149f74c42498bea
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 09:48:03 +0530

    Change the main axis size and center the elements

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -104,6 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Container(
           color: Colors.black38,
           child: Row(
+            mainAxisSize: MainAxisSize.min,
             children: [
               Container(
                 height: 100.0,
