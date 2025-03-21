6950632e41cb5117a050dc11a35c9650c1776d34
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 09:34:10 +0530

    Row

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             Container(
