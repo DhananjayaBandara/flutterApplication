d1b680b9b192928f4aea499a435d984906ae15b8
DhananjayaBandara <145431367+DhananjayaBandara@users.noreply.github.com>
2025-03-21 09:33:07 +0530

    Take as less place as possible in the main axis by the column widget. This is the way to put the children of column into center.

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Container(
               height: 100.0,
